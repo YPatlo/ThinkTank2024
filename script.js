61cd27b38d8799ce81f9ab89d87d090a634c526c
YPatlo <patloyashwanthkumar2@gmail.com>
2024-01-09 21:43:45 +0530

    updated styles

--- script.js
+++ script.js
@@ -1,117 +1,178 @@
-"use strict";
+//References
+let timeLeft = document.querySelector(".time-left");
+let quizContainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-button");
+let countOfQuestion = document.querySelector(".number-of-question");
+let displayContainer = document.getElementById("display-container");
+let scoreContainer = document.querySelector(".score-container");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+let startScreen = document.querySelector(".start-screen");
+let startButton = document.getElementById("start-button");
+let questionCount;
+let scoreCount = 0;
+let count = 11;
+let countdown;
 
-const quizData = [
- {
-  question: "Where is the correct place to insert a JavaScript?",
-  a: "The <head> section",
-  b: "The <body> section",
-  c: "Both the <head> and the <body> section are correct",
-  d: "none of the above",
-  correct: "c"
- },
+//Questions and Options array
 
- {
-  question: "Which language runs in a web browser?",
-  a: "Java",
-  b: "C",
-  c: "Python",
-  d: "JavaScript",
-  correct: "d"
- },
- {
-  question: "What does CSS stand for?",
-  a: "Central Style Sheets",
-  b: "Cascading Style Sheets",
-  c: "Cascading Simple Sheets",
-  d: "Cars SUVs Sailboats",
-  correct: "b"
- },
- {
-  question: "What does HTML stand for?",
-  a: "Hypertext Markup Language",
-  b: "Hypertext Markdown Language",
-  c: "Hyperloop Machine Language",
-  d: "Helicopters Terminals Motorboats Lamborginis",
-  correct: "a"
- },
- {
-  question: "What year was JavaScript launched?",
-  a: "1996",
-  b: "1995",
-  c: "1994",
-  d: "none of the above",
-  correct: "b"
- }
+const quizArray = [
+  {
+    id: "0",
+    question: "Which is the most widely spoken language in the world?",
+    options: ["Spanish", "Mandarin", "English", "German"],
+    correct: "Mandarin",
+  },
+  {
+    id: "1",
+    question: "Which is the only continent in the world without a desert?",
+    options: ["North America", "Asia", "Africa", "Europe"],
+    correct: "Europe",
+  },
+  {
+    id: "2",
+    question: "Who invented Computer?",
+    options: ["Charles Babbage", "Henry Luce", "Henry Babbage", "Charles Luce"],
+    correct: "Charles Babbage",
+  },
 ];
 
-const quiz = document.querySelector(".quiz-body");
-const answerEl = document.querySelectorAll(".answer");
-const questionEl = document.getElementById("question");
-const footerEl = document.querySelector(".quiz-footer");
-const quizDetailEl = document.querySelector(".quiz-details");
-const liEl = document.querySelector("ul li");
-
-const a_txt = document.getElementById("a_text");
-const b_txt = document.getElementById("b_text");
-const c_txt = document.getElementById("c_text");
-const d_txt = document.getElementById("d_text");
-const btnSubmit = document.getElementById("btn");
-
-let currentQuiz = 0;
-let score = 0;
-
-loadQuiz();
-
-function loadQuiz() {
- deselectAnswers();
- const currentQuizData = quizData[currentQuiz];
- questionEl.innerText = currentQuizData.question;
- a_txt.innerText = currentQuizData.a;
- b_txt.innerText = currentQuizData.b;
- c_txt.innerText = currentQuizData.c;
- d_txt.innerText = currentQuizData.d;
- quizDetailEl.innerHTML = `<p>${currentQuiz + 1} of ${quizData.length}</p>`;
-}
-
-// deselect
-function deselectAnswers() {
- answerEl.forEach((answerEl) => {
-  answerEl.checked = false;
- });
-}
-
-// get selected
-function getSelected() {
- let answer;
- answerEl.forEach((answerEls) => {
-  if (answerEls.checked) {
-   answer = answerEls.id;
-  }
- });
- return answer;
-}
-
-btnSubmit.addEventListener("click", function () {
- const answers = getSelected();
-
- if (answers) {
-  if (answers === quizData[currentQuiz].correct) {
-   score++;
-  }
-  nextQuestion();
- }
+//Restart Quiz
+restart.addEventListener("click", () => {
+  initial();
+  displayContainer.classList.remove("hide");
+  scoreContainer.classList.add("hide");
 });
 
-// next Slide
-function nextQuestion() {
- currentQuiz++;
+//Next Button
+nextBtn.addEventListener(
+  "click",
+  (displayNext = () => {
+    //increment questionCount
+    questionCount += 1;
+    //if last question
+    if (questionCount == quizArray.length) {
+      //hide question container and display score
+      displayContainer.classList.add("hide");
+      scoreContainer.classList.remove("hide");
+      //user score
+      userScore.innerHTML =
+        "Your score is " + scoreCount + " out of " + questionCount;
+    } else {
+      //display questionCount
+      countOfQuestion.innerHTML =
+        questionCount + 1 + " of " + quizArray.length + " Question";
+      //display quiz
+      quizDisplay(questionCount);
+      count = 11;
+      clearInterval(countdown);
+      timerDisplay();
+    }
+  })
+);
 
- if (currentQuiz < quizData.length) {
-  loadQuiz();
- } else {
-  quiz.innerHTML = `<h2>You answered ${score}/${quizData.length} question correctly</h2>
-    <button type="button" onclick="location.reload()">Reload</button>
+//Timer
+const timerDisplay = () => {
+  countdown = setInterval(() => {
+    count--;
+    timeLeft.innerHTML = `${count}s`;
+    if (count == 0) {
+      clearInterval(countdown);
+      displayNext();
+    }
+  }, 1000);
+};
+
+//Display quiz
+const quizDisplay = (questionCount) => {
+  let quizCards = document.querySelectorAll(".container-mid");
+  //Hide other cards
+  quizCards.forEach((card) => {
+    card.classList.add("hide");
+  });
+  //display current question card
+  quizCards[questionCount].classList.remove("hide");
+};
+
+//Quiz Creation
+function quizCreator() {
+  //randomly sort questions
+  quizArray.sort(() => Math.random() - 0.5);
+  //generate quiz
+  for (let i of quizArray) {
+    //randomly sort options
+    i.options.sort(() => Math.random() - 0.5);
+    //quiz card creation
+    let div = document.createElement("div");
+    div.classList.add("container-mid", "hide");
+    //question number
+    countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
+    //question
+    let question_DIV = document.createElement("p");
+    question_DIV.classList.add("question");
+    question_DIV.innerHTML = i.question;
+    div.appendChild(question_DIV);
+    //options
+    div.innerHTML += `
+    <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
+     <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
+      <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
+       <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
     `;
-  footerEl.style.display = "none";
- }
+    quizContainer.appendChild(div);
+  }
 }
+
+//Checker Function to check if option is correct or not
+function checker(userOption) {
+  let userSolution = userOption.innerText;
+  let question =
+    document.getElementsByClassName("container-mid")[questionCount];
+  let options = question.querySelectorAll(".option-div");
+
+  //if user clicked answer == correct option stored in object
+  if (userSolution === quizArray[questionCount].correct) {
+    userOption.classList.add("correct");
+    scoreCount++;
+  } else {
+    userOption.classList.add("incorrect");
+    //For marking the correct option
+    options.forEach((element) => {
+      if (element.innerText == quizArray[questionCount].correct) {
+        element.classList.add("correct");
+      }
+    });
+  }
+
+  //clear interval(stop timer)
+  clearInterval(countdown);
+  //disable all options
+  options.forEach((element) => {
+    element.disabled = true;
+  });
+}
+
+//initial setup
+function initial() {
+  quizContainer.innerHTML = "";
+  questionCount = 0;
+  scoreCount = 0;
+  count = 11;
+  clearInterval(countdown);
+  timerDisplay();
+  quizCreator();
+  quizDisplay(questionCount);
+}
+
+//when user click on start button
+startButton.addEventListener("click", () => {
+  startScreen.classList.add("hide");
+  displayContainer.classList.remove("hide");
+  initial();
+});
+
+//hide quiz and display start screen
+window.onload = () => {
+  startScreen.classList.remove("hide");
+  displayContainer.classList.add("hide");
+};
